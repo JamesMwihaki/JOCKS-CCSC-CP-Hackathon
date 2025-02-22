0b1531459f1a4e249a06dfa08c9b86335df3b480
sageaadam <sageadeleadam@gmail.com>
2025-02-22 11:05:12 -0600

    communal colors in common.css page.

--- client/src/pages/sage.jsx
+++ client/src/pages/sage.jsx
@@ -1,7 +1,12 @@
+import './common.css';
+import './sage.css';
+
 export default function Sage() {
     return(
         <>
         <h1>This is Sage's page</h1>
+        <p>test</p>
         </>
     )
 }
+
