17d02a45e71d964fd330c7754f44b811da8203c2
kford834 <kford4095@gmail.com>
2025-02-22 14:43:53 -0600

    database changes

--- client/src/pages/kevin.jsx
+++ client/src/pages/kevin.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useRef, useState } from "react"
+import axios from "axios";
+
 export default function Kevin() {
+    const isMounted = useRef(false)
+    useEffect(() => {
+        if (!isMounted.current) {
+            axios.post(`http://localhost:3000`, 
+                {
+                    id: 2, // id is the primary column. Each row needs a unique number.
+                    info: "message"
+                }
+            )
+            axios.get(`http://localhost:3000`).then((res) => {
+                console.log(res)
+                //console.log(res.data)
+            })
+        }
+        isMounted.current = true;
+    }, [])
+
     return(
         <>
         <h1>This is Kevin's page</h1>
