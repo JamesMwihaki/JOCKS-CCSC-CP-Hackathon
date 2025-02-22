b347c4409c34bf99932cb21e0fcfbe3927a881ac
Collin Downey <collin.downeymn@gmail.com>
2025-02-22 12:26:12 -0600

    Partial search implementation

--- client/src/pages/collin.jsx
+++ client/src/pages/collin.jsx
@@ -1,6 +1,12 @@
+// Author: Collin Downey
+// Date: 02/22/2025
+// Description: A test page for making the filter and search bar
 import './collin.css';
+import 'list.js';
 
 export default function Collin() {
+    // Toggles the visibility of dropdown menu items
+    // Takes an integer to select which dropdown to use
     function toggleDropdown(id) {
         let dropdownElements = document.getElementsByClassName('dropdown')[id].getElementsByClassName('dropdown-content');
         if (getComputedStyle(dropdownElements[1]).getPropertyValue('display') == 'none') {
@@ -16,10 +22,27 @@ export default function Collin() {
         }
     }
 
+    // Does a fuzzy search for rooms and buildings, then presents the results
+    // Takes the current search
+    function search(terms) {
+        var options = {
+            valueNames: ['Cunningham', 'Osborne', 'Bell'],
+            fuzzySearch: {
+                searchClass: "fuzzy-search",
+                location: 0,
+                distance: 100,
+                threshold: 0.4,
+                multiSearch: true
+            }
+        };
+        var listObj = new List('list-id',options);
+        console.log(listObj.fuzzySearch('ham'));
+    }
+
     return(
         <>
             <ul className='navbar'>
-                <input type="text" className="search"></input>
+                <input type="text" className="search" placeholder="&#x1F50D;&#xFE0E; Search" onChange={(e) => search(e)}></input>
                 <li className="dropdown">
                     <p onClick={() => toggleDropdown(0)} className="filter">Buildings</p>
                     <p className="dropdown-content">Cunningham Center</p>
