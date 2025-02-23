4cce4e103c373fb48d22a5e07887cefdecd885d0
kford834 <kford4095@gmail.com>
2025-02-23 16:46:16 -0600

    code

--- client/src/pages/james.jsx
+++ client/src/pages/james.jsx
@@ -1,20 +1,82 @@
-import React from 'react';
-import { MapContainer, ImageOverlay, useMap } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React, { useRef, useState }  from 'react';
+import { MapContainer, TileLayer, Popup, Rectangle, useMapEvents, Marker} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Leaflet's default styles
+import './james.css'
 
-import imageUrl from './Untitled.png';
+export default function James() {
+    const center = [38.87680, -94.78253];
+    const bounds = [
+        [38.87630, -94.78213], // Southwest corner
+        [38.87720, -94.78293]  // Northeast corner
+    ];
+    const bounds_Lunn_Hall = [
+      [38.87460, -94.78193],
+      [38.87495, -94.78233]
+    ];
 
-const MyMap = () => {
-  const bounds = [
-    [40.712, -74.227], // Top left coordinates
-    [40.774, -74.125], // Bottom right coordinates
-  ];
 
-    return (
-      <MapContainer center={[40.743, -74.176]} zoom={13} style={{ height: '500px', width: '400px' }}>
-        <ImageOverlay url={imageUrl} bounds={bounds} />
-      </MapContainer>
-    );
-};
+    const mapRef = useRef();
+    
+    const handleMapClick = (e) => {
+      const lat = e.latlng.lat;
+      const lng = e.latlng.lng;
+      console.log('hi')
+      const map = mapRef.current;
+      if (map) {
+        map.setView([lat, lng], map.getZoom())
+      }
+    };
 
-export default MyMap;
+    const [showSidebar, setShowSidebar] = useState(false)
+    const [showSidebar2, setShowSidebar2] = useState(false)
+    const [name, setName] = useState("")
+
+    const handleLocationClick = (buildingTitle) => {
+      setShowSidebar(true);
+
+      setName(buildingTitle) ;
+    };
+
+  return (
+    <>
+    <div className='home_page'>
+      <div className='map_container'>
+        <MapContainer center={center} zoom={15} style={{ height: '100%', width: '100%' }} whenCreated={(map) => { mapRef.current = map }} onClick={handleMapClick}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+          />
+            <Rectangle
+              bounds={bounds}
+              pathOptions={{ color: 'blue', fillOpacity: 0.3, fillColor: 'blue' }}
+              eventHandlers = {{click: () => handleLocationClick("Bell Cultural Events Center")}}
+            >
+              <Popup>
+                <strong>Bell Center </strong>
+              </Popup>
+            </Rectangle>
+
+            <Rectangle
+              bounds={bounds_Lunn_Hall}
+              pathOptions={{ color: 'blue', fillOpacity: 0.3, fillColor: 'blue' }}
+              eventHandlers = {{click: () => handleLocationClick("Lunn Hall")}}
+            >
+              <Popup>
+                <strong>Lunn Hall </strong>
+              </Popup>
+            </Rectangle>
+
+        </MapContainer>
+      </div>
+
+      {showSidebar && showSidebar2 && (
+        <div className="side_bar" >
+          <h3>{name}</h3>
+          <p> </p>
+          <button onClick={() => setShowSidebar(false)}>Close</button>
+        </div>
+      )}
+    </div>
+    </>    
+  );
+}
