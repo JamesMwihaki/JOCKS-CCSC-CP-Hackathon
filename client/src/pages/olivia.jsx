a889841f81d68ec9a6daf2eac8f485818dfdc8a4
Olivia <orford04@gmail.com>
2025-02-23 15:36:21 -0600

    Interior view stoof

--- client/src/pages/olivia.jsx
+++ client/src/pages/olivia.jsx
@@ -1,7 +1,38 @@
+import React from "react";
+import { MapContainer, ImageOverlay, useMap, Circle, Rectangle, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
+import imageUrl from '../images/LayoutBell.png'
+
 export default function Olivia() {
-    return(
-        <>
-        <h1>This is Olivia's page</h1>
-        </>
-    )
-}
+    const bounds = [
+        [40.704, -74.220],
+        [40.782, -74.125],
+    ];
+
+    /*const room_button = [*/
+    const position = [40.743, -74.176]
+    const radius = 100
+
+    const handleLocationClick = (roomTitle) => {
+
+    };
+
+
+    return (
+        <MapContainer center={[40.743, -74.176]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+            <ImageOverlay url={imageUrl} bounds={bounds}/>
+            <Circle 
+            center={position} 
+            radius={radius} 
+            color='blue' 
+            fillOpacity={0.3} 
+            fillColor='blue'
+            eventHandlers = {{click: () => handleLocationClick("Mabee Performing Arts Hall")}}
+            >
+            <Popup>
+                <strong>101</strong>
+            </Popup>
+            </Circle>
+        </MapContainer>
+    );
+};
